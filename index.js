83fbd83b80ff77466cf7beb564f8e2adf49cdbe4
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2020-05-05 11:45:01 -0500

    Setup

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv/config');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+//location of frontend
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+//location of APIs
+/* app.use('/', require('./apis/groceryList'));
+app.use('/', require('./apis/item')); */
+
+app.listen(PORT, () => {console.log(`Server started on ${PORT}`);});
